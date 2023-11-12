e35f95f48a1ec4cf5ef6efce3c8356eadbb2ac81
Jieun Lee <jieun98.dev@gmail.com>
2023-11-12 21:53:12 +0900

    [#1] init: project

---
+++ src/main/java/com/spotlightkonex/SpotlightkonexBackApplication.java
@@ -0,0 +1,13 @@
+package com.spotlightkonex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlightkonexBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotlightkonexBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spotlightkonex/SpotlightkonexBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotlightkonex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlightkonexBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
