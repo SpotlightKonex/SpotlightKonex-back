3d3b79f23cefc02be6d4d670cfd40b2c6a221b5a
2522001 <minseo770@gmail.com>
2023-11-17 20:39:50 +0900

    [#5] feat: 뉴스 엔티티 추가

---
+++ src/main/java/com/spotlightkonex/news/domain/entity/News.java
@@ -0,0 +1,40 @@
+package com.spotlightkonex.news.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String link;
+
+    @Column
+    private String description;
+
+    @Column
+    private String pubDate;
+
+    @Builder
+    public News(Long id, String title, String link, String description, String pubDate) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.pubDate = pubDate;
+    }
+
+}
