3cc793d142f3e66ef2cae5dd1e12e98d32880dc3
Jieun Lee <jieun98.dev@gmail.com>
2023-11-17 19:15:02 +0900

    [#9] feat: EnterpriseDto 추가

---
+++ src/main/java/com/spotlightkonex/Enterprise/controller/dto/EnterpriseDto.java
@@ -0,0 +1,9 @@
+package com.spotlightkonex.Enterprise.controller.dto;
+
+public interface EnterpriseDto {
+    String getCorpCode();
+    String getCordName();
+    String getLogo();
+    Long getPrice();
+    Long getPrevPrice();
+}
