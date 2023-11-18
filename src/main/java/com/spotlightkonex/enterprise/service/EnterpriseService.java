1048f752122248ae8d34c418ea1def427dab58a0
Jieun Lee <jieun98.dev@gmail.com>
2023-11-18 13:57:45 +0900

    [#11] feat: 랭크 타입 별 top11 조회 API

--- src/main/java/com/spotlightkonex/enterprise/service/EnterpriseService.java
+++ src/main/java/com/spotlightkonex/enterprise/service/EnterpriseService.java
@@ -3,6 +3,6 @@ package com.spotlightkonex.enterprise.service;
 import org.springframework.http.ResponseEntity;
 
 public interface EnterpriseService {
-
    ResponseEntity<?> getEnterprise();
+   ResponseEntity<?> getEnterpriseByRankTpye(String rankType);
 }
