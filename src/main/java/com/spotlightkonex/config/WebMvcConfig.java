4a1b825fc37f0c8284b84bdc2da9ec7cbc88462f
Jieun Lee <jieun98.dev@gmail.com>
2023-11-19 14:36:04 +0900

    [#1] fix: WebMvcConfig 위치 수정

--- src/main/java/com/spotlightkonex/config/WebMvcConfig.java
+++ src/main/java/com/spotlightkonex/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.spotlightkonex;
+package com.spotlightkonex.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
