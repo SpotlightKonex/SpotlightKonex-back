30b61a158a350811be16b9cac2a3d784bf3627fd
Jieun Lee <jieun98.dev@gmail.com>
2023-11-19 13:18:59 +0900

    [#1] feat: WebMvcConfig 추가

---
+++ src/main/java/com/spotlightkonex/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.spotlightkonex;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //모든 경로에 대해서
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*") // 기타 설정
+                .allowedHeaders("*");
+    }
+}
