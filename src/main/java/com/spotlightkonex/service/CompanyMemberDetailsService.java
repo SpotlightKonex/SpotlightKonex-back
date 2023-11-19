69217dbf9add2f380ec9447cadb635cc6c529f7c
ghdeo <goldencorn7@naver.com>
2023-11-20 08:53:02 +0900

    [#30] feat: UserDetailsService 구현체 CompanyMemberDetailsService 추가

---
+++ src/main/java/com/spotlightkonex/service/CompanyMemberDetailsService.java
@@ -0,0 +1,24 @@
+package com.spotlightkonex.service;
+
+import com.spotlightkonex.domain.entity.CompanyMember;
+import com.spotlightkonex.domain.entity.CompanyMemberDetails;
+import com.spotlightkonex.repository.CompanyMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyMemberDetailsService implements UserDetailsService {
+    private final CompanyMemberRepository companyMemberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        CompanyMember companyUser = companyMemberRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("회원 정보 를 찾지 못했습니다."));
+        return CompanyMemberDetails.of(companyUser);
+    }
+}
