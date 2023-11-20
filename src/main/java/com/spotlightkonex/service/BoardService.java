9727a72a0949cd24af2f5e4ab6150ece3eae65a4
Seoyun0626 <tjdbs0626@gmail.com>
2023-11-20 17:25:30 +0900

    [#6] feat: 게시판 인가 제거
    [#6] feat: 게시판 인가 제거

--- src/main/java/com/spotlightkonex/service/BoardService.java
+++ src/main/java/com/spotlightkonex/service/BoardService.java
@@ -1,14 +1,12 @@
 package com.spotlightkonex.service;
 
-import com.spotlightkonex.domain.dto.BoardDeleteRequestDto;
 import com.spotlightkonex.domain.dto.BoardPutRequestDto;
 import com.spotlightkonex.domain.dto.BoardRequestDto;
-import com.spotlightkonex.security.CompanyMemberDetails;
 import org.springframework.http.ResponseEntity;
 
 public interface BoardService {
-    ResponseEntity<?> createBoard(CompanyMemberDetails companyMemberDetails, BoardRequestDto requestDto);
+    ResponseEntity<?> createBoard(BoardRequestDto requestDto);
     ResponseEntity<?> searchBoards(String corpCode);
-    ResponseEntity<?> updateBoard(CompanyMemberDetails companyMemberDetails, BoardPutRequestDto requestDto);
-    ResponseEntity<?> deleteBoard(CompanyMemberDetails companyMemberDetails, BoardDeleteRequestDto requestDto);
+    ResponseEntity<?> updateBoard(BoardPutRequestDto requestDto);
+    ResponseEntity<?> deleteBoard(Long noticeSeq);
 }
--- src/main/java/com/spotlightkonex/service/BoardServiceImpl.java
+++ src/main/java/com/spotlightkonex/service/BoardServiceImpl.java
@@ -1,13 +1,9 @@
 package com.spotlightkonex.service;
 
-import com.spotlightkonex.domain.dto.BoardDeleteRequestDto;
 import com.spotlightkonex.domain.dto.BoardListResponseDto;
 import com.spotlightkonex.domain.dto.BoardPutRequestDto;
-import com.spotlightkonex.domain.entity.CompanyMember;
 import com.spotlightkonex.domain.entity.KonexStock;
-import com.spotlightkonex.repository.CompanyMemberRepository;
 import com.spotlightkonex.repository.KonexStockRepository;
-import com.spotlightkonex.security.CompanyMemberDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,26 +22,13 @@ import java.util.List;
 public class BoardServiceImpl implements BoardService {
     private final BoardRepository boardRepository;
     private final KonexStockRepository konexStockRepository;
-    private final CompanyMemberRepository companyMemberRepository;
 
     @Override
-    public ResponseEntity<?> createBoard(CompanyMemberDetails companyMemberDetails, BoardRequestDto requestDto) {
+    public ResponseEntity<?> createBoard(BoardRequestDto requestDto) {
         try {
-            // 로그인 여부 체크
-            if(companyMemberDetails == null) //로그인이 안된 사용자일 때
-                return ResponseEntity.noContent().build();
-            // 로그인 된 사용자
-            CompanyMember companyMember = companyMemberRepository.findByKonexStockCorpCode(requestDto.getCorpCode())
-                    .orElseThrow(() -> new NullPointerException("해당하는 기업코드가 존재하지 않습니다."));
-            String reqeustEmail = companyMember.getEmail();
-
-            if(!companyMemberDetails.getEmail().equals(reqeustEmail)) //해당하는 담당자(로그인된 사용자 email vs corpCode를 통한 email)
-                return ResponseEntity.noContent().build();
-
-            String userCorpCode = companyMemberDetails.getCorpCode();
-            KonexStock konexStock = konexStockRepository.findByCorpCode(userCorpCode)
+            String corpCode = requestDto.getCorpCode(); //requestDto에 있는 corp_code의 값을 cropCode에 담기
+            KonexStock konexStock = konexStockRepository.findByCorpCode(corpCode)
                     .orElseThrow(() -> new NullPointerException("잘못된 기업 코드입니다.")); //KonexStock 표 전체 내용(해당기업)
-
             Board board = Board.builder()
                     .title(requestDto.getTitle())
                     .context(requestDto.getContext())
@@ -91,20 +74,8 @@ public class BoardServiceImpl implements BoardService {
     // 게시물 수정
     @Override
     @Transactional
-    public ResponseEntity<?> updateBoard(CompanyMemberDetails companyMemberDetails, BoardPutRequestDto requestDto) {
+    public ResponseEntity<?> updateBoard(BoardPutRequestDto requestDto) {
         try {
-
-            // 로그인 여부 체크
-            if(companyMemberDetails == null) //로그인이 안된 사용자일 때
-                return ResponseEntity.noContent().build();
-            // 로그인 된 사용자
-            CompanyMember companyMember = companyMemberRepository.findByKonexStockCorpCode(requestDto.getCorpCode())
-                    .orElseThrow(() -> new NullPointerException("해당하는 기업코드가 존재하지 않습니다."));
-            String reqeustEmail = companyMember.getEmail();
-
-            if(!companyMemberDetails.getEmail().equals(reqeustEmail)) //해당하는 담당자
-                return ResponseEntity.noContent().build();
-
             Board board = boardRepository.findByNoticeSeq(requestDto.getNoticeSeq());
 
             if (board == null) {
@@ -135,32 +106,18 @@ public class BoardServiceImpl implements BoardService {
     // 게시물 삭제
     @Override
     @Transactional
-    public ResponseEntity<?> deleteBoard(CompanyMemberDetails companyMemberDetails, BoardDeleteRequestDto requestDto) {
+    public ResponseEntity<?> deleteBoard(Long noticeSeq) {
         try {
-
-            // 로그인 여부 체크
-            if(companyMemberDetails == null) //로그인이 안된 사용자일 때
-                return ResponseEntity.noContent().build();
-            // 로그인 된 사용자
-            CompanyMember companyMember = companyMemberRepository.findByKonexStockCorpCode(requestDto.getCorpCode())
-                    .orElseThrow(() -> new NullPointerException("해당하는 기업코드가 존재하지 않습니다."));
-            String reqeustEmail = companyMember.getEmail();
-
-            if(!companyMemberDetails.getEmail().equals(reqeustEmail)) //해당하는 담당자
-                return ResponseEntity.noContent().build();
-
-            Board board = boardRepository.findByNoticeSeq(requestDto.getNoticeSeq());
+            Board board = boardRepository.findByNoticeSeq(noticeSeq);
 
             if (board == null) {
                 ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body("해당 번호의 게시물이 존재하지 않습니다.");// 404(Not Found)
             }
-            boardRepository.deleteById(requestDto.getNoticeSeq());
+            boardRepository.deleteById(noticeSeq);
             return new ResponseEntity<>("게시물 삭제 성공", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("게시물 삭제 실패", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
-
-
