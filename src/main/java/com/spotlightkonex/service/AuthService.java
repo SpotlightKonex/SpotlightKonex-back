9884495d1af6dfcd4d5261d05644a619df0162ee
ghdeo <goldencorn7@naver.com>
2023-11-20 09:15:00 +0900

    [#30] feat: AuthService 추가

---
+++ src/main/java/com/spotlightkonex/service/AuthService.java
@@ -0,0 +1,70 @@
+package com.spotlightkonex.service;
+
+import com.spotlightkonex.domain.dto.*;
+import com.spotlightkonex.domain.entity.CompanyMember;
+import com.spotlightkonex.repository.CompanyMemberRepository;
+import com.spotlightkonex.security.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final TokenProvider tokenProvider;
+    private final CompanyMemberRepository companyMemberRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Long signUp(final CompanyMemberDto companyMemberDto) {
+        CompanyMember companyMember = CompanyMember.builder()
+                .corpCode(companyMemberDto.getCorpCode())
+                .email(companyMemberDto.getEmail())
+                .password(passwordEncoder.encode(companyMemberDto.getPassword()))
+                .corpName(companyMemberDto.getCorpName())
+                .phone(companyMemberDto.getPhone())
+                .corpCode(companyMemberDto.getCorpCode())
+                .corpAuth(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        CompanyMember save = companyMemberRepository.save(companyMember);
+
+        return save.getMemberSeq();
+    }
+
+    public SignInResponseDto signIn(final SignInRequestDto signInRequestDto) {
+        CompanyMember companyUser = companyMemberRepository
+                .findByEmail(signInRequestDto.getEmail())
+                .orElseThrow();
+
+        if (!passwordEncoder.matches(signInRequestDto.getPassword(), companyUser.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+
+        return SignInResponseDto.builder()
+                .email(signInRequestDto.getEmail())
+                .accessToken(tokenProvider.createToken(signInRequestDto.getEmail()))
+                .build();
+    }
+
+    public SignOutResponseDto signOut(final SignOutRequestDto signOutRequestDto) {
+        if (!tokenProvider.validateToken(signOutRequestDto.getAccessToken())) {
+            System.out.println("잘못된 요쳥");
+        }
+
+        // 해당 Access Token 유효 시간을 가지고 와서 BlackList 로 저장하기
+        Long expiration = tokenProvider.getExpiration(signOutRequestDto.getAccessToken());
+        redisTemplate.opsForValue()
+                .set(signOutRequestDto.getAccessToken(), "logout", expiration, TimeUnit.MILLISECONDS);
+
+        return SignOutResponseDto.builder()
+                .email(signOutRequestDto.getEmail())
+                .build();
+    }
+}
