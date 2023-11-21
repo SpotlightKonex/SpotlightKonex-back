626e3bd0686be9dd1154039495eedc426ea97c46
ghdeo <goldencorn7@naver.com>
2023-11-21 14:00:35 +0900

    [#78] feat: getSectorBalanceSheetByCorpCode 추가

--- src/main/java/com/spotlightkonex/service/BalanceSheetService.java
+++ src/main/java/com/spotlightkonex/service/BalanceSheetService.java
@@ -5,6 +5,7 @@ import com.spotlightkonex.domain.entity.BalanceSheet;
 import com.spotlightkonex.exception.CustomException;
 import com.spotlightkonex.exception.ErrorCode;
 import com.spotlightkonex.repository.BalanceSheetRepository;
+import com.spotlightkonex.repository.KonexStockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BalanceSheetService {
     private final BalanceSheetRepository balanceSheetRepository;
+    private final KonexStockRepository konexStockRepository;
+    private final FindService findService;
 
     public BalanceSheetResponseDto getBalanceSheetByCorpCode(String corpCode) {
         BalanceSheet balanceSheet = balanceSheetRepository.findByKonexStock_CorpCode(corpCode)
@@ -27,5 +30,7 @@ public class BalanceSheetService {
                 .build();
     }
 
+    public void getSectorBalanceSheetByCorpCode(Long theme) {
 
+    }
 }
