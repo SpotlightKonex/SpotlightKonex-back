6a7ee768349107fd4dd9f7e21577dc9c21396645
ghdeo <goldencorn7@naver.com>
2023-11-21 13:39:11 +0900

    [#78] feat: BalanceSheetService 개별 재무제표 조회 추가

--- src/main/java/com/spotlightkonex/service/BalanceSheetService.java
+++ src/main/java/com/spotlightkonex/service/BalanceSheetService.java
@@ -1,5 +1,9 @@
 package com.spotlightkonex.service;
 
+import com.spotlightkonex.domain.dto.BalanceSheetResponseDto;
+import com.spotlightkonex.domain.entity.BalanceSheet;
+import com.spotlightkonex.exception.CustomException;
+import com.spotlightkonex.exception.ErrorCode;
 import com.spotlightkonex.repository.BalanceSheetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +12,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class BalanceSheetService {
     private final BalanceSheetRepository balanceSheetRepository;
+
+    public BalanceSheetResponseDto getBalanceSheetByCorpCode(String corpCode) {
+        BalanceSheet balanceSheet = balanceSheetRepository.findByKonexStock_CorpCode(corpCode)
+                .orElseThrow(() -> new CustomException(ErrorCode.COMPANY_NOT_FOUND));
+
+        return BalanceSheetResponseDto.builder()
+                .corpCode(balanceSheet.getKonexStock().getCorpCode())
+                .div(balanceSheet.getDiv())
+                .bps(balanceSheet.getBps())
+                .per(balanceSheet.getPer())
+                .eps(balanceSheet.getEps())
+                .pbr(balanceSheet.getPbr())
+                .build();
+    }
 
 
 }
